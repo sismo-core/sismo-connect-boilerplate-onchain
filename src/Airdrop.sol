c9ba3451a005ff285baf22823aff25453619ed24
Benjamin A <97291322+Baoufa@users.noreply.github.com>
2023-06-22 01:59:04 +0200

    feat: better comment in contract

--- src/Airdrop.sol
+++ src/Airdrop.sol
@@ -8,71 +8,85 @@ import "sismo-connect-solidity/SismoLib.sol"; // <--- add a Sismo Connect import
 /*
  * @title Airdrop
  * @author Sismo
- * @dev Simple Airdrop contract that mints ERC20 tokens to the msg.sender
+ * @dev Simple Airdrop contract that mints ERC20 tokens to a receiver
  * This contract is used for tutorial purposes only
  * It will be used to demonstrate how to integrate Sismo Connect
  */
 contract Airdrop is ERC20, SismoConnect {
-    using SismoConnectHelper for SismoConnectVerifiedResult;
-    error UserNotEligibleForAirdrop();
+  using SismoConnectHelper for SismoConnectVerifiedResult;
+  error UserNotEligibleForAirdrop();
 
-    struct StoredClaim {
-        bytes16 groupId;
-        uint256 value;
-        bool claimed;
-    }
-// hello man
-    mapping(uint256 user => mapping(bytes16 groupId => StoredClaim)) public userClaims;
-    
-    bytes16 public constant GITCOIN_PASSPORT_GROUP_ID = 0x1cde61966decb8600dfd0749bd371f12;
-    bytes16 public constant SISMO_COMMUNITY_MEMBERS_GROUP_ID = 0xd630aa769278cacde879c5c0fe5d203c;
-    bytes16 public constant SISMO_COMMUNITY_EARLY_MEMBERS = 0xe4c011331d91b79639df349a93157a1b;
-    bytes16 public constant SISMO_FACTORY_USERS = 0x05629c9a54e30d8c8aea911a48cd9e30;
+  struct StoredClaim {
+    bytes16 groupId;
+    uint256 value;
+    bool claimed;
+  }
 
-    uint256 public constant REWARD_BASE_VALUE = 100 * 10 ** 18;
+  mapping(uint256 user => mapping(bytes16 groupId => StoredClaim)) public userClaims;
 
-    constructor(
-        string memory name,
-        string memory symbol,
-        bytes16 appId,
-        bool isImpersonationMode
-    ) ERC20(name, symbol) SismoConnect(buildConfig(appId, isImpersonationMode)) {}
+  bytes16 public constant GITCOIN_PASSPORT_GROUP_ID = 0x1cde61966decb8600dfd0749bd371f12;
+  bytes16 public constant SISMO_COMMUNITY_MEMBERS_GROUP_ID = 0xd630aa769278cacde879c5c0fe5d203c;
+  bytes16 public constant SISMO_COMMUNITY_EARLY_MEMBERS = 0xe4c011331d91b79639df349a93157a1b;
+  bytes16 public constant SISMO_FACTORY_USERS = 0x05629c9a54e30d8c8aea911a48cd9e30;
+  uint256 public constant REWARD_BASE_VALUE = 100 * 10 ** 18;
 
-    function _getRewardAmount(
-        SismoConnectVerifiedResult memory result,
-        uint256 userId
-    ) private returns (uint256) {
-        uint256 airdropAmount = 0;
+  constructor(
+    string memory name,
+    string memory symbol,
+    bytes16 appId,
+    bool isImpersonationMode
+  ) ERC20(name, symbol) SismoConnect(buildConfig(appId, isImpersonationMode)) {}
 
-        for (uint i = 0; i < result.claims.length; i++) {
-            VerifiedClaim memory verifiedClaim = result.claims[i];
-            bytes16 groupId = verifiedClaim.groupId;
+  function _getRewardAmount(
+    SismoConnectVerifiedResult memory result,
+    uint256 userId
+  ) private returns (uint256) {
+    uint256 airdropAmount = 0;
 
-            StoredClaim storage userClaim = userClaims[userId][groupId];
-            userClaim.groupId = groupId;
+    // we iterate over the claims returned by the Sismo Connect 
+    for (uint i = 0; i < result.claims.length; i++) {
+      VerifiedClaim memory verifiedClaim = result.claims[i];
+      bytes16 groupId = verifiedClaim.groupId;
 
-            if (groupId == SISMO_COMMUNITY_MEMBERS_GROUP_ID) {
-                bool isClaimable = verifiedClaim.value > userClaim.value;
-                if (isClaimable) {
-                    airdropAmount += (verifiedClaim.value - userClaim.value) * REWARD_BASE_VALUE;
-                    userClaim.claimed = true;
-                    userClaim.value = verifiedClaim.value;
-                    // store airdrop value
-                }
-            } else {
-                if (!userClaim.claimed) {
-                    airdropAmount += REWARD_BASE_VALUE;
-                    userClaim.claimed = true;
-                    userClaim.value = verifiedClaim.value;
-                    // store airdrop value
-                }
-            }
+      StoredClaim storage userClaim = userClaims[userId][groupId];
+      userClaim.groupId = groupId;
+
+      // we check if the user is eligible for the airdrop
+      if (groupId == SISMO_COMMUNITY_MEMBERS_GROUP_ID) {
+        bool isClaimable = verifiedClaim.value > userClaim.value;
+        if (isClaimable) {
+          // if the user is eligible, we store the claim and add the airdrop value
+          // for SISMO_COMMUNITY_MEMBERS_GROUP_ID, the value is level in the community
+          airdropAmount += (verifiedClaim.value - userClaim.value) * REWARD_BASE_VALUE;
+          userClaim.claimed = true;
+          userClaim.value = verifiedClaim.value;
+          // store airdrop value
         }
-        return airdropAmount;
+      } else {
+        if (!userClaim.claimed) {
+          // if the user is eligible, we store the claim and add the airdrop value
+          airdropAmount += REWARD_BASE_VALUE;
+          userClaim.claimed = true;
+          userClaim.value = verifiedClaim.value;
+          // store airdrop value
+        }
+      }
     }
+    return airdropAmount;
+  }
 
   function claimWithSismo(address receiver, bytes memory response) public {
+    // we want to verify 4 claims and 1 auth request
 
+    // we are recreating the auth request made in the frontend to be sure that
+    // the proofs provided in the response are valid with respect to this auth request
+    AuthRequest[] memory auths = new AuthRequest[](1);
+    auths[0] = buildAuth({authType: AuthType.VAULT});
+
+
+    // we want to verify 4 claims
+    // we are recreating the claims made in the frontend to be sure that
+    // the proofs provided in the response are valid with respect to these claims
     ClaimRequest[] memory claims = new ClaimRequest[](4);
     claims[0] = buildClaim({
       groupId: GITCOIN_PASSPORT_GROUP_ID,
@@ -95,14 +109,10 @@ contract Airdrop is ERC20, SismoConnect {
       isOptional: true
     });
 
-    AuthRequest[] memory auths = new AuthRequest[](1);
-    auths[0] = buildAuth({authType: AuthType.VAULT});
-
+    // we verify the response
     SismoConnectVerifiedResult memory result = verify({
       responseBytes: response,
       // we want the user to prove that he owns a Sismo Vault
-      // we are recreating the auth request made in the frontend to be sure that
-      // the proofs provided in the response are valid with respect to this auth request
       auths: auths,
       claims: claims,
       // we also want to check if the signed message provided in the response is the signature of the user's address
@@ -110,12 +120,12 @@ contract Airdrop is ERC20, SismoConnect {
     });
 
     // if the proofs and signed message are valid, we take the userId from the verified result
-    // in this case the userId is the vaultId (since we used AuthType.VAULT in the auth request), the anonymous identifier of a user's vault for a specific app --> vaultId = hash(userVaultSecret, appId)
+    // in this case the userId is the vaultId (since we used AuthType.VAULT in the auth request), the anonymous identifier of a user's vault for a specific app --> userId = hash(userVaultSecret, appId)
     uint256 userId = result.getUserId(AuthType.VAULT);
 
     //we get the airdrop amount from the verified result based on the number of claims and auths that were verified
     uint256 airdropAmount = _getRewardAmount(result, userId);
-  
+
     if (airdropAmount == 0) revert UserNotEligibleForAirdrop();
 
     // we mint the tokens to the user
