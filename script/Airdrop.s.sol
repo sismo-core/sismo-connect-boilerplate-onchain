64184b686adb2e09d9e73cfc0252f06b76f5be65
bigq <yum0e@protonmail.com>
2023-07-17 16:57:15 +0200

    feat: add tests

--- script/Airdrop.s.sol
+++ script/Airdrop.s.sol
@@ -2,16 +2,13 @@
 pragma solidity ^0.8.20;
 
 import "forge-std/Script.sol";
-import "forge-std/console.sol";
 import "sismo-connect-solidity/SismoLib.sol";
-import {SismoConnectConfigReader} from "./utils/SismoConnectConfigReader.sol";
+import {SismoConnectConfigReader} from "script/utils/SismoConnectConfigReader.sol";
 import {Airdrop} from "src/Airdrop.sol";
 
 contract DeployAirdrop is Script, SismoConnectConfigReader {
-  using stdJson for string;
-
   function run() public {
-    console.log("Deploying Airdrop contract");
+    console.log("Deploying Airdrop contract...");
     string memory json = vm.readFile(string.concat(vm.projectRoot(), "/sismo-connect-config.json"));
     (
       bytes16 appId,
