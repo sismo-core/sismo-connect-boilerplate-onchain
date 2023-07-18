7b6fa345a30ba067d764e7ea36fb5b65ef4d0e79
bigq <99990370+yum0e@users.noreply.github.com>
2023-07-18 22:27:36 +0200

    feat: typo frontend explaantions + fix chain error (#15)

--- front/src/app/components/Header.tsx
+++ front/src/app/components/Header.tsx
@@ -11,27 +11,24 @@ const Header: React.FC = () => {
         <a href="https://builders.sismo.io"> Join the Telegram Builders Group </a>
       </h3>
       <p>
-        1. The frontend requests ZK Proofs via Sismo Connect Button <br />
-        2. The user generates ZK Proofs in their Data Vault and sends the Sismo Connect response to
+        1. A contract is deployed with a Sismo Connect Configuration and a Sismo Connect Request <br />
+        2. The frontend queries the Sismo Connect Configuration and Request from the contract <br />
+        3. The frontend requests ZK Proofs via Sismo Connect Button <br />
+        4. The user generates ZK Proofs in their Data Vault and sends the Sismo Connect response to
         the frontend <br />
-        3. The frontend forwards the response to ERC20 smart contract via claimWithSismo function{" "}
+        5. The frontend forwards the response to ERC20 smart contract via claimWithSismo function{" "}
         <br />
-        4. The smart contract verifies the proofs contained in the response, mints ERC20 tokens and stores
+        6. The smart contract verifies the proofs contained in the response, mints ERC20 tokens and stores
         verified claims and auths <br />
-        5. The frontend reads the verified claims and auths from the contract and displays them
+        7. The frontend reads the verified claims and auths from the contract and displays them
       </p>
       <div>
         <p>
-          <b className="code-snippet">src/front/app/sismo-connect-config.ts</b>: Sismo Connect
-          configuration and requests
+          <b className="code-snippet">src/front/app/page.tsx</b>: Frontend - Queries contract to know Sismo Connect Configuration and Request, requests ZK Proofs from users via Sismo Connect Button
         </p>
         <p>
-          <b className="code-snippet">src/front/app/page.tsx</b>: Frontend - make Sismo Connect
-          request
-        </p>
-        <p>
-          <b className="code-snippet">src/Airdrop.sol</b>: Contract - verify Sismo Connect request,
-          mint tokens and stores verified claims and auths
+          <b className="code-snippet">src/Airdrop.sol</b>: Contract - Sets up the Sismo Connect Configuration and Request, verifies Sismo Connect response,
+          mints tokens and stores verified claims, auths and signed message
         </p>
         <p className="callout">
           {" "}
